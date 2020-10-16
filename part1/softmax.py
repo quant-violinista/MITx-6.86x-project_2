2a10f477e564778a44d2cf2b15f3c6eb00af3474
quant-violinista <quant.violinista@gmail.com>
2020-10-16 16:56:27 -0500

    Compute probabilities for Softmax

--- part1/softmax.py
+++ part1/softmax.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("..")
 import utils
 from utils import *
@@ -19,6 +20,7 @@ def augment_feature_vector(X):
     column_of_ones = np.zeros([len(X), 1]) + 1
     return np.hstack((column_of_ones, X))
 
+
 def compute_probabilities(X, theta, temp_parameter):
     """
     Computes, for each datapoint X[i], the probability that X[i] is labeled as j
@@ -31,8 +33,12 @@ def compute_probabilities(X, theta, temp_parameter):
     Returns:
         H - (k, n) NumPy array, where each entry H[j][i] is the probability that X[i] is labeled as j
     """
-    #YOUR CODE HERE
-    raise NotImplementedError
+    exponents = theta.dot(X.T) / temp_parameter
+    stability_factor = np.amax(exponents, axis=0).T
+    exponents = exponents - stability_factor
+    weights = np.exp(exponents)
+    return weights / np.sum(weights, axis=0)
+
 
 def compute_cost_function(X, Y, theta, lambda_factor, temp_parameter):
     """
@@ -50,8 +56,9 @@ def compute_cost_function(X, Y, theta, lambda_factor, temp_parameter):
     Returns
         c - the cost value (scalar)
     """
-    #YOUR CODE HERE
+    # YOUR CODE HERE
     raise NotImplementedError
+
 
 def run_gradient_descent_iteration(X, Y, theta, alpha, lambda_factor, temp_parameter):
     """
@@ -70,8 +77,9 @@ def run_gradient_descent_iteration(X, Y, theta, alpha, lambda_factor, temp_param
     Returns:
         theta - (k, d) NumPy array that is the final value of parameters theta
     """
-    #YOUR CODE HERE
+    # YOUR CODE HERE
     raise NotImplementedError
+
 
 def update_y(train_y, test_y):
     """
@@ -90,8 +98,9 @@ def update_y(train_y, test_y):
         test_y_mod3 - (n, ) NumPy array containing the new labels (a number between 0-2)
                     for each datapoint in the test set
     """
-    #YOUR CODE HERE
+    # YOUR CODE HERE
     raise NotImplementedError
+
 
 def compute_test_error_mod3(X, Y, theta, temp_parameter):
     """
@@ -108,8 +117,9 @@ def compute_test_error_mod3(X, Y, theta, temp_parameter):
     Returns:
         test_error - the error rate of the classifier (scalar)
     """
-    #YOUR CODE HERE
+    # YOUR CODE HERE
     raise NotImplementedError
+
 
 def softmax_regression(X, Y, temp_parameter, alpha, lambda_factor, k, num_iterations):
     """
@@ -140,6 +150,7 @@ def softmax_regression(X, Y, temp_parameter, alpha, lambda_factor, k, num_iterat
         theta = run_gradient_descent_iteration(X, Y, theta, alpha, lambda_factor, temp_parameter)
     return theta, cost_function_progression
 
+
 def get_classification(X, theta, temp_parameter):
     """
     Makes predictions by classifying a given dataset
@@ -156,13 +167,15 @@ def get_classification(X, theta, temp_parameter):
     """
     X = augment_feature_vector(X)
     probabilities = compute_probabilities(X, theta, temp_parameter)
-    return np.argmax(probabilities, axis = 0)
+    return np.argmax(probabilities, axis=0)
+
 
 def plot_cost_function_over_time(cost_function_history):
     plt.plot(range(len(cost_function_history)), cost_function_history)
     plt.ylabel('Cost Function')
     plt.xlabel('Iteration number')
     plt.show()
+
 
 def compute_test_error(X, Y, theta, temp_parameter):
     error_count = 0.
