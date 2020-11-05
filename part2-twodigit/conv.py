d48278c848d9d5868d79dbc7cc05c44c7daca2cb
quant-violinista <quant.violinista@gmail.com>
2020-11-05 14:17:25 -0600

    Implement double digit recognition
    -FF Neural network
    -Convolutional neural network architecture

--- part2-twodigit/conv.py
+++ part2-twodigit/conv.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from train_utils import batchify_data, run_epoch, train_model, Flatten
 import utils_multiMNIST as U
+
 path_to_data_dir = '../Datasets/'
 use_mini_dataset = True
 
@@ -11,21 +12,33 @@ batch_size = 64
 nb_classes = 10
 nb_epoch = 30
 num_classes = 10
-img_rows, img_cols = 42, 28 # input image dimensions
-
+img_rows, img_cols = 42, 28  # input image dimensions
 
 
 class CNN(nn.Module):
 
     def __init__(self, input_dimension):
         super(CNN, self).__init__()
-        # TODO initialize model layers here
+        self.conv1 = nn.Conv2d(1, 32, (3, 3))
+        self.pool1 = nn.MaxPool2d((2, 2))
+        self.conv2 = nn.Conv2d(32, 64, (3, 3))
+        self.pool2 = nn.MaxPool2d((2, 2))
+        self.flatten = Flatten()
+        self.fc1 = nn.Linear(2880, 64)
+        self.drop = nn.Dropout()
+        self.fc2 = nn.Linear(64, 20)
 
     def forward(self, x):
-
-        # TODO use model layers to predict the two digits
-
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        xf = self.flatten(x)
+        xf = F.relu(self.fc1(xf))
+        xf = self.drop(xf)
+        xf = self.fc2(xf)
+        out_first_digit = xf[:, :10]
+        out_second_digit = xf[:, :-10]
         return out_first_digit, out_second_digit
+
 
 def main():
     X_train, y_train, X_test, y_test = U.get_data(path_to_data_dir, use_mini_dataset)
@@ -49,14 +62,16 @@ def main():
 
     # Load model
     input_dimension = img_rows * img_cols
-    model = CNN(input_dimension) # TODO add proper layers to CNN class above
+    model = CNN(input_dimension)
 
     # Train
     train_model(train_batches, dev_batches, model)
 
     ## Evaluate the model on test data
     loss, acc = run_epoch(test_batches, model.eval(), None)
-    print('Test loss1: {:.6f}  accuracy1: {:.6f}  loss2: {:.6f}   accuracy2: {:.6f}'.format(loss[0], acc[0], loss[1], acc[1]))
+    print('Test loss1: {:.6f}  accuracy1: {:.6f}  loss2: {:.6f}   accuracy2: {:.6f}'.format(loss[0], acc[0], loss[1],
+                                                                                            acc[1]))
+
 
 if __name__ == '__main__':
     # Specify seed for deterministic behavior, then shuffle. Do not change seed for official submissions to edx
